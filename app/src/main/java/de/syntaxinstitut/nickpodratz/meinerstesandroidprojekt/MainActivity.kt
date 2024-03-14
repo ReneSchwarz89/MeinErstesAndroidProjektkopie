0c6c1e8e780789feb99f167c03a83a53f0d13656
nickpodratz <nick.podratz@syntax-institut.de>
2024-03-14 14:46:04 +0100

    Add "slide to purchase" slider

--- app/src/main/java/de/syntaxinstitut/nickpodratz/meinerstesandroidprojekt/MainActivity.kt
+++ app/src/main/java/de/syntaxinstitut/nickpodratz/meinerstesandroidprojekt/MainActivity.kt
@@ -2,10 +2,13 @@ package de.syntaxinstitut.nickpodratz.meinerstesandroidprojekt
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ncorti.slidetoact.SlideToActView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val slider = findViewById<SlideToActView>(R.id.slider)
     }
 }
